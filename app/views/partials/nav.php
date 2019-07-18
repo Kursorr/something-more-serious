edcb9302010886e943da93f7a5c6a09c49f3a272
Ravaniss <senechal.max06@gmail.com>
2019-07-18 14:56:35 +0200

    nav - add signin link

--- app/views/partials/nav.php
+++ app/views/partials/nav.php
@@ -5,7 +5,7 @@
         <div class="logo"></div>
         <ul>
           <li><a href="/"><span>Home</span></a></li>
-          <li><a href="/about"><span>Sign in</span></a></li>
+          <li><a href="/signin"><span>Sign in</span></a></li>
           <li><a href="/contact"><span>Contact</span></a></li>
           <li><a href="/users"><span>Manage Users</span></a></li>
         </ul>

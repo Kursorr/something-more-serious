6fc8474c5504a572a99f8f80df3ec28d0e766f05
Ravaniss <senechal.max06@gmail.com>
2019-07-18 15:22:40 +0200

    Sign in view - Add error if error

--- app/views/signin.view.php
+++ app/views/signin.view.php
@@ -1,13 +1,18 @@
 <?php require('partials/head.php'); ?>
   <div class="outContent">
     <div class="content">
-      <form action="/signin" method="post" class="signin">
-        <input type="text" name="pseudo" placeholder="Your pseudo">
-        <input type="email" name="email" placeholder="Your Email">
-        <input type="password" name="password" placeholder="Your password">
-        <input type="password" name="verif-password" placeholder="Verif password">
-        <button type="submit">S'inscrire</button>
-      </form>
+      <div class="container">
+        <form action="/signin" method="post" class="signin">
+          <input type="text" name="pseudo" placeholder="Your pseudo">
+          <input type="email" name="email" placeholder="Your Email">
+          <input type="password" name="password" placeholder="Your password">
+          <input type="password" name="verifpassword" placeholder="Verif password">
+          <button type="submit">S'inscrire</button>
+          <?php if ($error !== ''): ?>
+            <?= $error; ?>
+          <?php endif ?>
+        </form>
+      </div>
     </div>
   </div>
 <?php require('partials/footer.php'); ?>

3975c528ec2d36cdb1a34ac9c60f81fd2dc7aace
Ravaniss <senechal.max06@gmail.com>
2019-07-18 14:12:14 +0200

    Footer updated

--- app/views/partials/footer.php
+++ app/views/partials/footer.php
@@ -1,2 +1,9 @@
+  <div class="outContent">
+    <div class="content">
+      <div id="footer">
+        A little footer ! :) For fun - Something More Serious :kappa:
+      </div>
+    </div>
+  </div>
 </body>
 </html>

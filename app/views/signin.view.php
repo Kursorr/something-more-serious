ea0bbb8a5563832c54c932a657e64451fea1d703
Ravaniss <senechal.max06@gmail.com>
2019-07-18 14:56:48 +0200

    Create signin view

--- app/views/signin.view.php
+++ app/views/signin.view.php
@@ -1,3 +1,13 @@
 <?php require('partials/head.php'); ?>
-  <h1>About <?= $company; ?></h1>
+  <div class="outContent">
+    <div class="content">
+      <form action="/signin" method="post" class="signin">
+        <input type="text" name="pseudo" placeholder="Your pseudo">
+        <input type="email" name="email" placeholder="Your Email">
+        <input type="password" name="password" placeholder="Your password">
+        <input type="password" name="verif-password" placeholder="Verif password">
+        <button type="submit">S'inscrire</button>
+      </form>
+    </div>
+  </div>
 <?php require('partials/footer.php'); ?>

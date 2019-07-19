941eef1fb5a5755ea485d37f96dcd71f41c1897d
Ravaniss <senechal.max06@gmail.com>
2019-07-19 10:58:47 +0200

    Sign up view updated

--- app/views/signin.view.php
+++ app/views/signin.view.php
@@ -3,10 +3,10 @@
     <div class="content">
       <div class="container">
         <form action="/signin" method="post" class="signin">
-          <input type="text" name="pseudo" placeholder="Your pseudo">
-          <input type="email" name="email" placeholder="Your Email">
-          <input type="password" name="password" placeholder="Your password">
-          <input type="password" name="verifpassword" placeholder="Verif password">
+          <input type="text" name="pseudo" placeholder="Your pseudo" required>
+          <input type="email" name="email" placeholder="Your Email" required>
+          <input type="password" name="password" placeholder="Your password" required>
+          <input type="password" name="verifpassword" placeholder="Verif password" required>
           <button type="submit">S'inscrire</button>
           <?php if ($error !== ''): ?>
             <?= $error; ?>

5d294d402e09ec6471287a0c6f991d192e058a66
Ravaniss <senechal.max06@gmail.com>
2019-07-20 12:09:47 +0200

    Update pages controller

--- app/controllers/PagesController.php
+++ app/controllers/PagesController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controllers;
 
+use App\Core\App;
+
 class PagesController
 {
   /**
@@ -9,7 +11,8 @@ class PagesController
    */
   public function home()
   {
-    return view('index');
+    $pseudo = '';
+    return view('index', ['pseudo' => $pseudo]);
   }
 
   /**
@@ -20,14 +23,14 @@ class PagesController
     return view('contact');
   }
 
-  /**
-   * Show the signin page
-   */
-  public function signin()
+
+  public function signup()
   {
     $error = '';
 
-    return view('signin', ['error' => $error]);
+    return view('signup', [
+      'error' => $error
+    ]);
   }
 
   /**
@@ -42,12 +45,62 @@ class PagesController
 
     $error = '';
 
-    if (strlen($pseudo) <= 4) {
+    if (strlen($pseudo) < 4) {
       $error = 'Your pseudo should contains more or equal to 4 characters.';
 
-      return view('signin', ['error' => $error]);
+      return view('signin', ['errorRegister' => $error]);
     }
 
-    return true;
+    if (empty($email)) {
+      $error = 'You need an email';
+
+      return view('signin', ['errorRegister' => $error]);
+    }
+
+    if ((empty($password) || empty($verifpassword) || ($password !== $verifpassword)) ) {
+      $error = 'Passwords should be equal';
+
+      return view('signin', ['errorRegister' => $error]);
+    }
+
+    App::get('database')->insert('users', [
+      'pseudo' => $pseudo,
+      'email' => $email,
+      'password' => password_hash($password, PASSWORD_BCRYPT, ['cost' => 12]),
+      'created_at' => date("Y-m-d H:i:s")
+    ]);
+
+    $error = 'Account created with success !!';
+    return view('signin', ['error' => $error]);
   }
+
+  public function signin()
+  {
+    $error = '';
+
+    return view('signin', [
+      'error' => $error
+    ]);
+  }
+
+  /**
+   * Show the signin page
+   */
+  public function login()
+  {
+    $pseudo = $_POST['pseudo'];
+    $password = $_POST['password'];
+
+
+
+    session_start();
+
+    $_SESSION['logged'] = true;
+    $_SESSION['pseudo'] = $pseudo;
+
+    var_dump($_SESSION);
+
+    return view('index', ['pseudo' => $_SESSION['pseudo']]);
+  }
+
 }

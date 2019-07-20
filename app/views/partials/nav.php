ba6a1d818eb83fe6c8672b199a2a2df8d753a743
Ravaniss <senechal.max06@gmail.com>
2019-07-20 17:22:51 +0200

    Nav - updated with logout

--- app/views/partials/nav.php
+++ app/views/partials/nav.php
@@ -5,8 +5,12 @@
         <div class="logo"></div>
         <ul>
           <li><a href="/"><span>Home</span></a></li>
-          <li><a href="/signup"><span>Sign up</span></a></li>
-          <li><a href="/signin"><span>Sign in</span></a></li>
+          <?php if ($_SESSION['pseudo']): ?>
+            <li><a href="/logout"><span>Log out</span></a></li>
+          <?php else: ?>
+            <li><a href="/signup"><span>Sign up</span></a></li>
+            <li><a href="/signin"><span>Sign in</span></a></li>
+          <?php endif ?>
           <li><a href="/contact"><span>Contact</span></a></li>
           <li><a href="/users"><span>Manage Users</span></a></li>
         </ul>

dda2d853127cbc0571bfbdaf28b0a10a4587d32c
Ravaniss <senechal.max06@gmail.com>
2019-07-20 12:11:26 +0200

    update users view

--- app/views/users.view.php
+++ app/views/users.view.php
@@ -1,15 +1,17 @@
+<?php session_start() ?>
 <?php require('partials/head.php'); ?>
-  <h1>All Users</h1>
 
-<?php foreach ($users as $user) : ?>
-    <li><?= $user->name; ?></li>
-<?php endforeach; ?>
+<div class="outContent">
+  <div class="content">
+    <?php if ($_SESSION['pseudo']): ?>
+      connected as <?= $_SESSION['pseudo'] ?>
+    <?php endif ?>
+    <h1>All Users</h1>
 
-<h1>Submit Your Name</h1>
-
-<form method="POST" action="/users">
-    <input name="name"></input>
-    <button type="submit">Submit</button>
-</form>
+    <?php foreach ($users as $user) : ?>
+      <li><?= $user->pseudo; ?></li>
+    <?php endforeach; ?>
+  </div>
+</div>
 
 <?php require('partials/footer.php'); ?>

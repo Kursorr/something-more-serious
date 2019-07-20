51e0e24d30b82c329ab2d4e1b0fa2e2d87d4ed17
Ravaniss <senechal.max06@gmail.com>
2019-07-20 12:09:32 +0200

    signup view

---
+++ app/views/signup.view.php
@@ -0,0 +1,18 @@
+<?php require('partials/head.php'); ?>
+<div class="outContent">
+  <div class="content">
+    <div class="container">
+      <form action="/signup" method="post" class="signin">
+        <input type="text" name="pseudo" placeholder="Your pseudo" required>
+        <input type="email" name="email" placeholder="Your Email" required>
+        <input type="password" name="password" placeholder="Your password" required>
+        <input type="password" name="verifpassword" placeholder="Verif password" required>
+        <button type="submit">S'inscrire</button>
+        <?php if ($error !== ''): ?>
+          <?= $error; ?>
+        <?php endif ?>
+      </form>
+    </div>
+  </div>
+</div>
+<?php require('partials/footer.php'); ?>

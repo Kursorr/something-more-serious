b8867815afbfcd906ae63c0392c573b7bb52a4a6
Ravaniss <senechal.max06@gmail.com>
2019-07-20 12:10:05 +0200

    Update userscontroller - maybe delete this

--- app/controllers/UsersController.php
+++ app/controllers/UsersController.php
@@ -15,16 +15,4 @@ class UsersController
 
     return view('users', compact('users'));
   }
-
-  /**
-   * Store a new user in the database.
-   */
-  public function store()
-  {
-    App::get('database')->insert('users', [
-      'name' => $_POST['name']
-    ]);
-
-    return redirect('users');
-  }
 }

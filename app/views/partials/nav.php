b652c2608d9168966c7e7461e45406b8d92a54bb
Ravaniss <senechal.max06@gmail.com>
2019-07-20 12:10:50 +0200

    Update navbar with signup / signin

--- app/views/partials/nav.php
+++ app/views/partials/nav.php
@@ -5,6 +5,7 @@
         <div class="logo"></div>
         <ul>
           <li><a href="/"><span>Home</span></a></li>
+          <li><a href="/signup"><span>Sign up</span></a></li>
           <li><a href="/signin"><span>Sign in</span></a></li>
           <li><a href="/contact"><span>Contact</span></a></li>
           <li><a href="/users"><span>Manage Users</span></a></li>

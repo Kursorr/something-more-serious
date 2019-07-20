0f391a22394d5d7a73da28f717d04026c1fe07af
Ravaniss <senechal.max06@gmail.com>
2019-07-20 12:10:29 +0200

    update index

--- app/views/index.view.php
+++ app/views/index.view.php
@@ -1,3 +1,7 @@
-<?php require('partials/head.php'); ?>
+<?php if (session_status() === 2) session_start() ?>
+<?php require('partials/head.php');?>
   <h1>Home Page</h1>
+  <?php if ($_SESSION['pseudo']): ?>
+    Welcome <?= $_SESSION['pseudo'] ?>
+  <?php endif ?>
 <?php require('partials/footer.php'); ?>

1a4b9b42fee6f860a4a8d4ba7443a150494addba
Ravaniss <senechal.max06@gmail.com>
2019-07-20 12:11:08 +0200

    Update signin view

--- app/views/signin.view.php
+++ app/views/signin.view.php
@@ -1,18 +1,16 @@
 <?php require('partials/head.php'); ?>
-  <div class="outContent">
-    <div class="content">
-      <div class="container">
-        <form action="/signin" method="post" class="signin">
-          <input type="text" name="pseudo" placeholder="Your pseudo" required>
-          <input type="email" name="email" placeholder="Your Email" required>
-          <input type="password" name="password" placeholder="Your password" required>
-          <input type="password" name="verifpassword" placeholder="Verif password" required>
-          <button type="submit">S'inscrire</button>
-          <?php if ($error !== ''): ?>
-            <?= $error; ?>
-          <?php endif ?>
-        </form>
-      </div>
+<div class="outContent">
+  <div class="content">
+    <div class="container">
+      <form action="/signin" method="post" class="signin">
+        <input type="text" name="pseudo" placeholder="Your pseudo" required>
+        <input type="password" name="password" placeholder="Your password" required>
+        <button type="submit">S'inscrire</button>
+        <?php if ($error !== ''): ?>
+          <?= $error; ?>
+        <?php endif ?>
+      </form>
     </div>
   </div>
+</div>
 <?php require('partials/footer.php'); ?>

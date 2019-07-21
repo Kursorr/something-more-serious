9448bfb3ce5d4faa779b935fa4b67251c7b06219
Ravaniss <senechal.max06@gmail.com>
2019-07-22 00:21:58 +0200

    SigninController - add auth from PagesController

--- app/controllers/SigninController.php
+++ app/controllers/SigninController.php
@@ -6,13 +6,13 @@ use App\Core\App;
 
 class SigninController
 {
-  /**
-   * Show the signin page
-   * @return mixed
-   */
-  public function signin()
+  public function signup()
   {
-    return view('signin');
+    $error = '';
+
+    return view('signup', [
+      'error' => $error
+    ]);
   }
 
   /**
@@ -27,10 +27,63 @@ class SigninController
 
     $error = '';
 
-    if (!strlen($pseudo) >= 4) {
-      return $error = 'Your pseudo should contains more or equal to 4 characters.';
+    if (strlen($pseudo) < 4) {
+      $error = 'Your pseudo should contains more or equal to 4 characters.';
+
+      return view('signin', ['errorRegister' => $error]);
     }
 
-    return true;
+    if (empty($email)) {
+      $error = 'You need an email';
+
+      return view('signin', ['errorRegister' => $error]);
+    }
+
+    if ((empty($password) || empty($verifpassword) || ($password !== $verifpassword)) ) {
+      $error = 'Passwords should be equal';
+
+      return view('signin', ['errorRegister' => $error]);
+    }
+
+    App::get('database')->insert('users', [
+      'pseudo' => $pseudo,
+      'email' => $email,
+      'password' => password_hash($password, PASSWORD_BCRYPT, ['cost' => 12]),
+      'created_at' => date("Y-m-d H:i:s")
+    ]);
+
+    $error = 'Account created with success !!';
+    return view('signin', ['error' => $error]);
+  }
+
+  public function signin()
+  {
+    $error = '';
+
+    return view('signin', [
+      'error' => $error
+    ]);
+  }
+
+  /**
+   * Show the signin page
+   */
+  public function login()
+  {
+    $pseudo = $_POST['pseudo'];
+    $password = $_POST['password'];
+
+    $res = App::get('database')->selectWhere();
+    var_dump($res);
+
+    $_SESSION['pseudo'] = $pseudo;
+
+    redirect('/');
+  }
+
+  public function logout()
+  {
+    unset($_SESSION['pseudo']);
+    return view('index');
   }
 }

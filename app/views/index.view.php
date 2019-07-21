d9f17e0b118fdb615120a37fc4679b859c8d8fd7
Ravaniss <senechal.max06@gmail.com>
2019-07-21 21:22:07 +0200

    index - fix conditional

--- app/views/index.view.php
+++ app/views/index.view.php
@@ -1,6 +1,6 @@
 <?php require('partials/head.php');?>
   <h1>Home Page</h1>
-  <?php if (isset($_SESSION) && $_SESSION['pseudo']): ?>
+  <?php if (!empty($_SESSION['pseudo'])): ?>
     Welcome <?= $_SESSION['pseudo'] ?>
   <?php endif ?>
 <?php require('partials/footer.php'); ?>
